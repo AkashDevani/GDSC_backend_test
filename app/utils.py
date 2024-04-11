346ddf92d65a4d2a342ed5855942e5ba7f6e15a6
AkashDevani <140954568+AkashDevani@users.noreply.github.com>
2024-04-11 19:08:30 +0530

    Create utils.py

---
+++ app/utils.py
@@ -0,0 +1,23 @@
+from flask import request, jsonify
+from functools import wraps
+import jwt
+import datetime
+from .models import User
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 403
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(id=data['id']).first()
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 403
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
